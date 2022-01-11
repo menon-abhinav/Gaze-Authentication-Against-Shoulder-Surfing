1f0eec0331bdfb1d5f842e933b8920030940793f
Pillai Manish <pillaimanish00@gmail.com>
2022-01-11 18:38:01 +0530

    Video for trial

--- Trial Code/main.py
+++ Trial Code/main.py
@@ -3,16 +3,16 @@ import cv2
 scale_factor = 1.2
 min_neighbors = 3
 min_size = (50, 50)
-webcam=True #if working with video file then make it 'False'
+webcam=False #if working with video file then make it 'False'
 
 def detect():
     face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     eye_classifier = cv2.CascadeClassifier('haarcascade_eye.xml')
-    if webcam:
-        video_cap = cv2.VideoCapture(0) # use 0,1,2..depanding on your webcam
+    # if webcam:
+    #     video_cap = cv2.VideoCapture(0) # use 0,1,2..depanding on your webcam
     
-    # else:
-    #     video_cap = cv2.VideoCapture("videoFile.mp4")
+    if True:
+        video_cap = cv2.VideoCapture("Present.mp4")
     while True:
         # Capture frame-by-frame
         ret, img = video_cap.read()
